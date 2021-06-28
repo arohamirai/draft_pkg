51c7cf0255fa7a6270defb264f94ab0d2ac1a48c
liufeng <wslf0115@gmail.com>
2021-06-28 13:38:28 +0800

    not important

---
+++ scripts/python_draft.py
@@ -0,0 +1,16 @@
+'''
+Author: liufeng(AT iplusbot.com)
+Date: 2021-06-24 15:25:13
+LastEditors: liufeng(AT iplusbot.com)
+LastEditTime: 2021-06-24 16:03:45
+Description: 
+'''
+import os
+import numpy as np
+
+
+mat = np.arange(12).reshape((3, 4))
+mat2 = mat[1:3, 1:3]
+
+print(mat)
+print(mat2)
